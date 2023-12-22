aef04ded5b4eb2e3666c9cf54efa528216b0e9be
DanilKulak <1998kylak@gmail.com>
2023-12-22 22:14:59 +0200

    commit-04-task-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,21 @@ import Profile from './Profile/Profile';
 import user from '../data/user.json';
 import Stats from './Stats/Stats';
 
-export const App = () => {
-  <>
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-/>
-<Stats
-  stats={user.stats}
-/>
-  </>
-}
+const App = () => {
+  return (
+    <>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+    />
+    <Stats
+      stats={user.stats}
+    />
+      </>
+  )
+    }
+
+
+export default App;
